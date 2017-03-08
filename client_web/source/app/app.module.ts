416faa009ef7dc1efb73978d0eda36ffc0cc618d
Gabriel Accettola <gabriel@accettolasystems.com>
2017-03-08 12:16:57 -0500

    - client web : app.module updated to include sample routes

--- client_web/source/app/app.module.ts
+++ client_web/source/app/app.module.ts
@@ -1,39 +1,55 @@
 
-import { NgModule }         from '@angular/core';
-import { BrowserModule }    from '@angular/platform-browser';
-import { FormsModule }      from '@angular/forms';
-import { MaterialModule }   from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { MaterialModule }       from '@angular/material';
+import { FlexLayoutModule }     from '@angular/flex-layout';
 
 import 'hammerjs';
 
-import { SidebarService }   from './core/sidebar/sidebar.service';
+import { AppRouting }           from './core/app.routing';
+import { LayoutService }        from './service/layout.service';
+import { SidebarService }       from './core/sidebar/sidebar.service';
 
-import { AppComponent }     from './core/app.component';
-import { HeaderComponent }  from './core/header/header.component';
-import { FooterComponent }  from './core/footer/footer.component';
-import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { AppComponent }         from './core/app.component';
+import { HeaderComponent }      from './core/header/header.component';
+import { FooterComponent }      from './core/footer/footer.component';
+import { SidebarComponent }     from './core/sidebar/sidebar.component';
 
-@NgModule( {
-    imports      :
-    [
-        BrowserModule
-    ,   FormsModule
-    ,   MaterialModule
-    ,   FlexLayoutModule
-    ],
-    declarations :
+
+import { DashboardComponent }   from './pages/dashboard/dashboard.component';
+import { MailComponent }        from './pages/mail/mail.component';
+import { FlagComponent }        from './pages/flag/flag.component';
+import { SettingsComponent }    from './pages/settings/settings.component';
+
+@NgModule (
+{
+    declarations    :
     [
         AppComponent
     ,   HeaderComponent
     ,   FooterComponent
     ,   SidebarComponent
-    ],
-    providers    :
+
+    ,   DashboardComponent
+    ,   MailComponent
+    ,   FlagComponent
+    ,   SettingsComponent
+    ]
+,   imports         :
     [
-        SidebarService
-    ],
-    bootstrap    :
+        BrowserModule
+    ,   FormsModule
+    ,   MaterialModule
+    ,   FlexLayoutModule
+    ,   AppRouting
+    ]
+,   providers       :
+    [
+        LayoutService
+    ,   SidebarService
+    ]
+,   bootstrap       :
     [
         AppComponent
     ]
