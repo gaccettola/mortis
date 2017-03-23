cae984ce6c48911c3222534f1afc797ffb73effd
Gabriel Accettola <gabriel@accettolasystems.com>
2017-03-23 02:26:23 -0400

    - restapi server 'release' directory removed

--- server_restapi/_clean.sh
+++ server_restapi/_clean.sh
@@ -10,10 +10,7 @@ echo cleaning server_restapi,
 rm -rf ./source/node_modules
 
 rm -rf ./debug
-rm -rf ./release
 
 mkdir -p ./debug/
-mkdir -p ./release/
 
 mkdir -p ./debug/common/
-mkdir -p ./release/common/
