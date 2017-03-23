b1a20f63ec478555b5b820853f26263aa1f5a84c
Gabriel Accettola <gabriel@accettolasystems.com>
2017-03-23 02:35:11 -0400

    - restapi server sample routines for 'active' removed, primus wrapper lib renamed

---
+++ server_restapi/source/common/constant_server_restapi.js
@@ -0,0 +1,25 @@
+
+( function ( window )
+{
+    'use strict';
+
+    var const_server_restapi =
+    {
+        restapi_listen      : 'restapi_listen',     // indicates that the http server should lifted
+        restapi_listening   : 'restapi_listening',  // indicates that the http server has lifted
+        scheduled_minute    : 'scheduled_minute'
+    };
+
+    // attach to 'module.exports' if it exists, else attach to 'window'.
+    // more robust : check that 'window' is a thing.
+
+    if ( 'undefined' !== typeof module && 'undefined' !== typeof module.exports )
+    {
+        module.exports = const_server_restapi;
+
+    } else
+    {
+        window.const_server_restapi = const_server_restapi;
+    }
+
+} ) ( this );
