badb4141f66523a88ca0bebea9d19d2a5524f0cf
Gabriel Accettola <gabriel@accettolasystems.com>
2017-04-16 00:30:31 -0400

    codemirror control added for markdown editor

---
+++ client_desktop/source/src/controls/ngCodeMirror/codemirror.component.ts
@@ -0,0 +1,117 @@
+// Imports
+import {
+    Component,
+    Input,
+    Output,
+    ElementRef,
+    ViewChild,
+    EventEmitter,
+    forwardRef,
+    AfterContentInit
+} from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import * as CodeMirror from 'codemirror';
+
+/**
+ * CodeMirror component
+ * Usage :
+ * <codemirror [(ngModel)]="data" [config]="{...}"></codemirror>
+ */
+@Component({
+    selector: 'codemirror',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => CodemirrorComponent),
+            multi: true
+        }
+    ],
+    templateUrl : './codemirror.component.html',
+    styleUrls   : ['./codemirror.component.scss']
+})
+export class CodemirrorComponent {
+
+    @Input() config;
+    @Output() change = new EventEmitter();
+    @Output() focus = new EventEmitter();
+    @Output() blur = new EventEmitter();
+
+    @ViewChild('host') host;
+
+    @Output() instance = null;
+
+    _value = '';
+
+    /**
+     * Constructor
+     */
+    constructor(){}
+
+    get value() { return this._value; };
+
+    @Input() set value(v) {
+        if (v !== this._value) {
+            this._value = v;
+            this.onChange(v);
+        }
+    }
+
+    /**
+     * On component destroy
+     */
+    ngOnDestroy(){
+
+    }
+
+    /**
+     * On component view init
+     */
+    ngAfterViewInit(){
+        this.config = this.config || {};
+        this.codemirrorInit(this.config);
+    }
+
+    /**
+     * Initialize codemirror
+     */
+    codemirrorInit(config){
+        this.instance = CodeMirror.fromTextArea(this.host.nativeElement, config);
+        this.instance.setValue(this._value);
+
+        this.instance.on('change', () => {
+            this.updateValue(this.instance.getValue());
+        });
+
+        this.instance.on('focus', () => {
+            this.focus.emit();
+        });
+
+        this.instance.on('blur', () => {
+            this.blur.emit();
+        });
+    }
+
+    /**
+     * Value update process
+     */
+    updateValue(value){
+        this.value = value;
+        this.onTouched();
+        this.change.emit(value);
+    }
+
+    /**
+     * Implements ControlValueAccessor
+     */
+    writeValue(value){
+        this._value = value || '';
+        if (this.instance) {
+            this.instance.setValue(this._value);
+        }
+    }
+    onChange(_){}
+    onTouched(){}
+    registerOnChange(fn){this.onChange = fn;}
+    registerOnTouched(fn){this.onTouched = fn;}
+
+}
